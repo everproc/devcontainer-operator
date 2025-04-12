f643ef3356458cb8ae6fbe8eb98a631268367801
jan <git@wiegelmann.net>
2025-04-12 22:16:38 +0200

    support mounts in bind mode

--- internal/parsing/devcontainerspec.go
+++ internal/parsing/devcontainerspec.go
@@ -37,17 +37,81 @@ func (cmd Cmd) MarshalJSON() ([]byte, error) {
 	return nil, errors.New("neither string nor array of cmd are defined, cannot marshal json")
 }
 
+// https://containers.dev/implementors/json_reference/
 type DevContainerSpec struct {
-	Name    string   `json:"name"`
-	Image   string   `json:"image,omitempty"`
+	// A name for the dev container displayed in the UI.
+	Name string `json:"name"`
+	// The name of an image in a container registry.
+	Image string `json:"image,omitempty"`
+	// An array of arguments that should be used when running the container.
 	RunArgs []string `json:"runArgs,omitempty"`
 	Build   struct {
-		Dockerfile string            `json:"dockerfile"`
-		Args       map[string]string `json:"args"`
+		// The location of a Dockerfile that defines the contents of the container.
+		Dockerfile string `json:"dockerfile"`
+		// A set of name-value pairs containing Docker image build arguments that should be passed when building a Dockerfile.
+		Args map[string]string `json:"args"`
 	} `json:"build,omitempty"`
 	Ports map[string]struct {
+		// Display name for the port in the ports view.
 		Label string `json:"label"`
 	} `json:"portsAttributes"`
+	// This command is the last of three that finalizes container setup when a dev container is created.
 	PostCreateCommand *Cmd `json:"postCreateCommand"`
-	PostStartCommand  *Cmd `json:"postStartCommand"`
+	// A command to run each time the container is successfully started.
+	PostStartCommand *Cmd `json:"postStartCommand"`
+	// Add additional mounts to a container.
+	Mounts []*Mount `json:"mounts,omitempty"`
+}
+
+// https://docs.docker.com/engine/storage/bind-mounts/#options-for---mount
+type Mount struct {
+	// `bind` mounts an existing persistentVolumeClaim
+	Type string `json:"type,omitempty"`
+	// The persistentVolumeClaim name.
+	Source string `json:"source,omitempty"`
+	// The path where the file or directory is mounted in the container. Must be an absolute path.
+	Target string `json:"target,omitempty"`
+	// If true, causes the bind mount to be mounted into the container as read-only. Default false.
+	Readonly bool `json:"readonly,omitempty"`
+}
+
+// https://docs.docker.com/engine/storage/bind-mounts/#options-for---mount
+func (m *Mount) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" || string(data) == `""` {
+		return nil
+	}
+	var j interface{}
+	if err := json.Unmarshal(data, &j); err != nil {
+		return err
+	}
+
+	if r, ok := j.(map[string]interface{}); ok {
+		if t, ok := r["type"].(string); ok {
+			m.Type = t
+		}
+
+		if source, ok := r["source"].(string); ok {
+			m.Source = source
+		} else if src, ok := r["src"].(string); ok {
+			m.Source = src
+		}
+
+		if target, ok := r["target"].(string); ok {
+			m.Target = target
+		} else if destination, ok := r["destination"].(string); ok {
+			m.Target = destination
+		} else if dst, ok := r["dst"].(string); ok {
+			m.Target = dst
+		}
+
+		if readonly, ok := r["readonly"].(bool); ok {
+			m.Readonly = readonly
+		} else if ro, ok := r["ro"].(bool); ok {
+			m.Readonly = ro
+		} else {
+			m.Readonly = false
+		}
+	}
+
+	return nil
 }
--- internal/parsing/devcontainerspec_test.go
+++ internal/parsing/devcontainerspec_test.go
@@ -3,6 +3,7 @@ package parsing_test
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 
 	"everproc.com/devcontainer/internal/parsing"
@@ -22,7 +23,14 @@ func TestAbc(t *testing.T) {
 			]
 		}
 	},
-	"postCreateCommand": "npm install -g @devcontainers/cli"
+	"postCreateCommand": "npm install -g @devcontainers/cli",
+	"mounts": [
+		{
+		"src": "dind-var-lib-docker",
+		"target": "/var/lib/docker",
+		"type": "bind"
+		}
+	]
 }`
 	x := &parsing.DevContainerSpec{}
 	err := json.Unmarshal([]byte(f), &x)
@@ -31,12 +39,73 @@ func TestAbc(t *testing.T) {
 		t.Fail()
 		return
 	}
-	fmt.Printf("Parsed: %+v", *x)
+	fmt.Printf("Unmarshal parsed: %+v", *x)
 	v, err := json.Marshal(x)
 	if err != nil {
 		fmt.Println(err)
 		t.Fail()
 		return
 	}
-	fmt.Printf("Parsed: %s", string(v))
+	fmt.Printf("Marshal parsed: %s", string(v))
+}
+
+func TestParseMounts(t *testing.T) {
+	const f = `{
+	"image": "mcr.microsoft.com/devcontainers/javascript-node:1-18-bullseye",
+	"features": {
+		"ghcr.io/devcontainers/features/docker-in-docker:2": {}
+	},
+	"customizations": {
+		"vscode": {
+			"extensions": [
+				"mads-hartmann.bash-ide-vscode",
+				"dbaeumer.vscode-eslint"
+			]
+		}
+	},
+	"postCreateCommand": "npm install -g @devcontainers/cli",
+	"mounts": [
+		{
+		"source": "dind-var-lib-docker",
+		"destination": "/var/lib/docker",
+		"type": "bind",
+		"readonly": true
+		},
+		{
+		"src": "dind-var-lib-docker",
+		"dst": "/var/lib/docker",
+		"type": "bind",
+		"ro": true
+		},
+		{
+		"src": "dind-var-lib-docker",
+		"target": "/var/lib/docker",
+		"type": "bind",
+		"readonly": true
+		}
+	]
+}`
+	result := parsing.DevContainerSpec{}
+	expected := parsing.Mount{
+		Source:   "dind-var-lib-docker",
+		Target:   "/var/lib/docker",
+		Type:     "volume",
+		Readonly: true,
+	}
+	err := json.Unmarshal([]byte(f), &result)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	if !reflect.DeepEqual(expected, *result.Mounts[0]) {
+		t.Errorf("parsing mounts was incorrect\ngot: %v\n\nwant: %v", *result.Mounts[0], expected)
+	}
+	if !reflect.DeepEqual(expected, *result.Mounts[1]) {
+		t.Errorf("parsing mounts was incorrect\ngot: %v\n\nwant: %v", *result.Mounts[1], expected)
+	}
+	if !reflect.DeepEqual(expected, *result.Mounts[2]) {
+		t.Errorf("parsing mounts was incorrect\ngot: %v\n\nwant: %v", *result.Mounts[2], expected)
+	}
 }
