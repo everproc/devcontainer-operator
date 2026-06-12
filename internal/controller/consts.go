1a394e9a87acf68efc3e845402838f650cff630f
Tobias Schäfer <github@blackox.org>
2026-06-12 14:51:06 +0200

    fix: release tag 0.0.10 (#55)

--- internal/controller/consts.go
+++ internal/controller/consts.go
@@ -11,12 +11,12 @@ var (
 func init() {
 	GIT_IMAGE_NAME = os.Getenv("GIT_IMAGE_NAME")
 	if GIT_IMAGE_NAME == "" {
-		GIT_IMAGE_NAME = "localhost:5001/git-clone:0.0.10"
+		GIT_IMAGE_NAME = "ghcr.io/everproc/git-clone:0.0.10"
 	}
 
 	PARSERAPP_IMAGE_NAME = os.Getenv("PARSERAPP_IMAGE_NAME")
 	if PARSERAPP_IMAGE_NAME == "" {
-		PARSERAPP_IMAGE_NAME = "localhost:5001/parserapp:0.0.10"
+		PARSERAPP_IMAGE_NAME = "ghcr.io/everproc/parserapp:0.0.10"
 	}
 
 	KANIKO_IMAGE_NAME = os.Getenv("KANIKO_IMAGE_NAME")
